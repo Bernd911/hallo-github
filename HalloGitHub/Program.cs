473397bb1732e086e973fcf303261c666183567f
Bernd Förster <ogv.foerster@gvbuero.de>
2017-11-20 21:43:24 +0100

    Zeile zugefügt

--- HalloGitHub/Program.cs
+++ HalloGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace HalloGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Der Test verlief gut!");
         }
     }
 }
